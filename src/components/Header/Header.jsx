f7c0c22451d9b3f91244601bf185f75f8ddd0118
Bart Frackiewicz <decomplosed@gmail.com>
2021-08-28 09:04:23 +0200

    imported useStyles and called it

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,11 @@ import { Autocomplete } from '@react-google-maps/api';
 import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 import { SearchIcon } from '@material-ui/icons';
 
+import useStyles from './styles';
+
 const Header = () => {
+  const classes = useStyles();
+
   return (
     <AppBar position='static'>
       <Toolbar className={classes.toolbar}>
