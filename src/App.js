cc14986b5d3176c04069bdae2c3b31272e98918b
Bart Frackiewicz <decomplosed@gmail.com>
2021-08-28 08:54:39 +0200

    render List

--- src/App.js
+++ src/App.js
@@ -8,7 +8,11 @@ const App = () => {
     <>
       <CssBaseline />
       <Header />
-      <Grid container spacing={3} style={{ width: '100%' }}></Grid>
+      <Grid container spacing={3} style={{ width: '100%' }}>
+        <Grid item xs={12} md={4}>
+          <List />
+        </Grid>
+      </Grid>
     </>
   );
 };
