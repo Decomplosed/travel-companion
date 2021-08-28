1f5f57c9cefea90b74985798ac833b590541ff16
Bart Frackiewicz <decomplosed@gmail.com>
2021-08-28 08:56:46 +0200

    imported material ui components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
 
 const Header = () => {
   return <div></div>;
