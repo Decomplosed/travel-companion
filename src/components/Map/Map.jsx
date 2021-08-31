5ee033494a6c7a3064cc1a3b4016eb853a0244f6
Bart Frackiewicz <decomplosed@gmail.com>
2021-08-31 21:42:12 +0200

    imported LocationOutlinedIcon

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOutlinedIcon from '@material-ui/icons/LocationCityOutlined';
 
 const Map = () => {
   return <div></div>;
