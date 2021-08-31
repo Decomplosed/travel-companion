7a94ee2b928b94d505d281000e38594a909e778f
Bart Frackiewicz <decomplosed@gmail.com>
2021-08-31 21:42:28 +0200

    imported Rating

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOutlinedIcon from '@material-ui/icons/LocationCityOutlined';
+import Rating from '@material-ui/lab';
 
 const Map = () => {
   return <div></div>;
