6eaf96bd4756dc06db9592eae1d0aa417a484eac
Bart Frackiewicz <decomplosed@gmail.com>
2021-08-31 22:00:12 +0200

    imported components from material ui

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,4 +1,13 @@
 import React from 'react';
+import {
+  CircularProgress,
+  Grid,
+  Typography,
+  InputLabel,
+  MenuItem,
+  FormControl,
+  Select,
+} from '@material-ui/core';
 
 const List = () => {
   return <div></div>;
