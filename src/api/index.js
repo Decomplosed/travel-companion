c4f5ae800e1d55ea221039ba88d727c98fe14f87
Bart Frackiewicz <decomplosed@gmail.com>
2021-08-31 22:20:26 +0200

    setup axios connection

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,32 @@
 import axios from 'axios';
 
+const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary',
+
+const options = {
+  method: 'GET',
+  url: URL,
+  params: {
+    bl_latitude: '11.847676',
+    tr_latitude: '12.838442',
+    bl_longitude: '109.095887',
+    tr_longitude: '109.149359',
+    restaurant_tagcategory_standalone: '10591',
+    restaurant_tagcategory: '10591',
+    limit: '30',
+    currency: 'USD',
+    open_now: 'false',
+    lunit: 'km',
+    lang: 'en_US'
+  },
+  headers: {
+    'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+    'x-rapidapi-key': '35bd102475msh323596ded40e0d3p1a49e4jsn49b3cb06b1c6'
+  }
+};
+
+
 const getPlacesData = async () => {
   try {
-    const response = axios.get();
+    const response = axios.get(URL, options);
   } catch (error) {}
 };
