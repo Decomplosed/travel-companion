a3ad52ff66ae09689dbcce345bac441cf857ce15
Bart Frackiewicz <decomplosed@gmail.com>
2021-09-01 13:30:27 +0200

    removed coordinates

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -10,8 +10,6 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width: 600px)');
 
-  const coordinates = { lat: 0, lng: 0 };
-
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
