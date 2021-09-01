7f0015058d361d3542a05d0137465d8b19b4728a
Bart Frackiewicz <decomplosed@gmail.com>
2021-09-01 13:31:59 +0200

    setup coordinates and bounds on map change

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -18,7 +18,10 @@ const Map = ({ setCoordinates, setBounds, coordinates }) => {
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
         options={''}
-        onChange={() => {}}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
         onChildClick={() => {}}
       ></GoogleMapReact>
     </div>
