1342ea86f85effa3ffca5cf5725c359d5cdf7135
Bart Frackiewicz <decomplosed@gmail.com>
2021-09-01 13:29:19 +0200

    passed setters as props

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+          <Map setCoordinates={setCoordinates} setBounds={setBounds} />
         </Grid>
       </Grid>
     </>
