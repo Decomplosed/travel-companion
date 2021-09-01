a34c2589055c91cfc43ed6da32c1813a3c5c96af
Bart Frackiewicz <decomplosed@gmail.com>
2021-09-01 13:19:16 +0200

    removed not needed options

--- src/api/index.js
+++ src/api/index.js
@@ -11,13 +11,6 @@ const options = {
     tr_latitude: '12.838442',
     bl_longitude: '109.095887',
     tr_longitude: '109.149359',
-    restaurant_tagcategory_standalone: '10591',
-    restaurant_tagcategory: '10591',
-    limit: '30',
-    currency: 'USD',
-    open_now: 'false',
-    lunit: 'km',
-    lang: 'en_US',
   },
   headers: {
     'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
